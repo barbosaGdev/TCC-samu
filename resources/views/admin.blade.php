5524b3b5b8da0bcb06f34ed290e619af1c7e21e4
Mysfer <jpfreitas2001@gmail.com>
2018-03-26 15:16:25 -0300

    alterações no menu admin

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -6,7 +6,15 @@
         <div class="col-md-8">
             <div class="card">
                 <h1>Bem Vindo Administrador!</h1>
+                <h2>O que deseja fazer?</h2>
+
+                <a href="{{ route('register') }}">Registrar novos Colaboradores</a>
+                <p>Lista de quem está realizando o cursos</p>
+
+                <a href="/adminOuvidoria">Ouvidoria</a>
                 
+                <a href="/adminAddCurso" >Adicionar novos cursos</a>
+
             </div>
         </div>
     </div>
