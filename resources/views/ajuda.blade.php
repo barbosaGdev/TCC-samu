63c48f8a56d494a2fe46baa4f1d8e5fef40567bd
barbosaGdev <biki.jam@hotmail.com>
2018-09-17 15:18:07 -0300

    manual de instrucao att 2

--- resources/views/ajuda.blade.php
+++ resources/views/ajuda.blade.php
@@ -18,14 +18,35 @@
     <br><br>
     <h4>Recebendo mensagens da Ouvidoria:</h4> <br>
         <img class="img-fluid rounded mb-4 animated fadeInRight" src="https://github.com/barbosaGdev/TCC-samu/blob/master/public/images/instrucao3.jpg?raw=true" alt="">
-        <p>Aqui será onde a comunidade fará suas devidas sugestões e críticas a respeito do atendimento. Vocês terão um recurso para saber como estão se saindo ao atender a população</p>
+        <p>Aqui será onde a comunidade fará suas devidas sugestões e críticas a respeito do atendimento. Vocês terão um recurso para saber como estão se saindo ao atender a população.</p>
 
     <hr>
 
     <br><br>
     <h4>Vizualizando feedback da comunidade:</h4> <br>
-        <img class="img-fluid rounded mb-4 animated fadeInRight" src="https://github.com/barbosaGdev/TCC-samu/blob/master/public/images/instrucao2.jpg?raw=true" alt="">
-        <p></p>
+        <img class="img-fluid rounded mb-4 animated fadeInRight" src="https://github.com/barbosaGdev/TCC-samu/blob/master/public/images/instrução2.PNG?raw=true" alt="">
+        <p>Nesta página poderá ser visualizado pelo administrador todas as mensagens de feedback da comunidade.</p>
+
+    <hr>
+
+    <br><br>
+    <h4>Vizualizando colaboradores:</h4> <br>
+        <img class="img-fluid rounded mb-4 animated fadeInRight" src="https://github.com/barbosaGdev/TCC-samu/blob/master/public/images/instrucao4.PNG?raw=true" alt="">
+        <p>Nesta página poderá ser visualizado pelo administrador todos os colaboradores registrados, e excluir algum colaborador clicando no ícone vermelho</p>
+
+    <hr>
+
+    <br><br>
+    <h4>Criando um curso:</h4> <br>
+        <img class="img-fluid rounded mb-4 animated fadeInRight" src="https://github.com/barbosaGdev/TCC-samu/blob/master/public/images/instrucao5.PNG?raw=true" alt="">
+        <p>Nesta página o administrador poderá criar um novo curso. Os campos de aulas complementares não são obrigatórios.</p>
+
+    <hr>
+
+    <br><br>
+    <h4>Criando uma notícia:</h4> <br>
+        <img class="img-fluid rounded mb-4 animated fadeInRight" src="https://github.com/barbosaGdev/TCC-samu/blob/master/public/images/instrucao6.PNG?raw=true" alt="">
+        <p>Nesta página o administrador poderá criar uma nova notícia que será exibida para a comunidade.</p>
 
     <hr>
 
