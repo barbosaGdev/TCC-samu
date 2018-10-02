f0ace81310c284e1562f861403adeb96245dd1af
MarrentaxD <jsouzajj27@gmail.com>
2018-10-02 16:52:51 -0300

    Página de Erro part 1

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<br><br>
+    
+    <div class="container">
+    <nav class="breadcrumb">
+    <a class="breadcrumb-item Texto" href="/">Home</a>
+    <span class="breadcrumb-item active Texto">Erro 404</span>
+    </nav>
+    <center>
+    <h1 class="Texto">Infelizmente Não Achamos a Página :(</h1>
+    <img class="img-fluid rounded mb-4 animated fadeInRight" src="https://github.com/barbosaGdev/TCC-samu/blob/master/public/images/ambulance.png?raw=true" alt="">
+    </div>
+     </center>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+@endsection
